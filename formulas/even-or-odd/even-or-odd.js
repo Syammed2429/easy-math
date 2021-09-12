5bc0e26dffd12ab1dd7403131554937e7cf456cc
Dada Khalandar <kerry.blig12@gmail.com>
2021-09-12 17:23:16 +0530

    added results text

--- formulas/even-or-odd/even-or-odd.js
+++ formulas/even-or-odd/even-or-odd.js
@@ -9,7 +9,7 @@ let res = document.querySelector('.results');
                     res.innerHTML = `<strong>Results</strong> : The entered number ${inputValue} is Even`;
                     how.innerHTML = `<strong>How</strong> : If a ${inputValue} is evenly divisible by 2 with no remainder, then it is even. You can calculate the remainder with the modulo operator % like this <code>${inputValue} % 2 == 0 </code>. If a ${inputValue} divided by 2 leaves a remainder of 1, then the ${inputValue} is odd.`
                 } else {
-                    res.innerHTML = `The entered number ${inputValue} is Odd`;
+                    res.innerHTML = `<strong>Results</strong> :The entered number ${inputValue} is Odd`;
                     how.innerHTML = `<strong>How</strong> : If a ${inputValue} divided by 2 leaves a remainder of 1, then the ${inputValue} is odd. If a ${inputValue} is evenly divisible by 2 with no remainder, then it is even. You can calculate the remainder with the modulo operator % like this <code>${inputValue} % 2 == 0</code> . `
                 }
             }
