86fcf13534706f79ba885c80f20bc86ebdfbceb7
* <shaianvaldez@gmail.com>
2021-09-20 21:46:20 +0800

    final design

--- main.js
+++ main.js
@@ -1,126 +1,334 @@
 const controller = new ScrollMagic.Controller();
+const scrollTopBtn = document.querySelector(".back-top");
+const facts = document.querySelector(".facts")
+const burgerMenu = document.querySelector(".hamburger-menu-container");
+const body = document.querySelector("body");
+let state = false;
 
-gsap.fromTo(".letter-title",{
-  y:"-2000%"
-},{
-  y:"0%",
-  duration:3,
-  stagger:1,
-  ease : "bounce.out"
-})
+burgerMenu.addEventListener("click",toggleMenu)
+function toggleMenu(){
+  if(!state){
+    burgerMenu.classList.add("open");
+    gsap.to(".navigation",{
+      x:"0%",
+      duration:0.3,
+      ease:"power4.out"
+    })
+    body.style.overflow = "hidden"
+    state = true
+  }
+  
+  else {
+    burgerMenu.classList.remove("open");
+    gsap.to(".navigation",{
+      x:"-120%",
+      duration:0.3,
+      ease:"power4.out"
+    })
+    body.style.overflow = "auto"
+    state = false
+  } 
+}
 
+/* TWEENING EXAMPLE TITLE */
 
-const firstSection = new TimelineMax()
-.fromTo(".section1",3,{
-  x:"-100%", 
-  opacity:0
+const exampleTitle = new TimelineMax()
+.fromTo(".example",0.3,{
+  x:"-150%",
+  ease:"power2.out", 
+  duration:0.3
 },{
   x:"0%",
-  ease : "Linear.easeOut", 
-  opacity:1
+  ease:"power2.out"
 })
-
-.fromTo(".books-body",2,{
-  y:"100%", 
-  scale:0.5
+.fromTo(".formula",0.3,{
+  x:"150%",
+  ease:"power2.out", 
+  duration:0.3
 },{
-  y:"-30%",
-  scale:1
-}) 
-.to(".navigation",2,{
-  opacity:0,
-  pointerEvents:"none"
+  x:"0%", 
+  ease:"0%"
 })
 
-const firstSectionTween = new ScrollMagic.Scene({
-  triggerHook:0.7, 
-  triggerElement:".body-section", 
+const titleTween = new ScrollMagic.Scene({
+  triggerHook:1,
+  triggerElement:".example-formula-container", 
   duration:500
 })
-.setTween(firstSection)
-.addTo(controller)
-/*  SQUARE FORMULA ANIMATION */
-const formulas = new TimelineMax()
-.fromTo(".rectangle-info",2,{
-  opacity:0,
-  scale:0
-},{
-  opacity:1,
-  scale:1, 
-  ease:"Linear.easeOut"
-})
-.fromTo(".rectangle-img",2,{
-  opacity:0,
-  x:"100%",
-  rotate:"180deg"
-},{
-  opacity:1,
-  rotate:"20deg",
-  x:"0%", 
-  ease:"Linear.easeOut"
-})
-
-const oddPrimeFormula = new TimelineMax()
-.fromTo(".even-odd-info",2,{
-  opacity:0,
-  scale:0
-},{
-  opacity:1,
-  scale:1, 
-  ease:"Linear.easeOut"
-})
-.fromTo(".even-odd-img",2,{
-  opacity:0,
-  x:"100%",
-  rotate:"180deg"
-},{
-  opacity:1,
-  rotate:"0deg",
-  x:"0%", 
-  ease:"Linear.easeOut"
-})
-
-const primeFormula = new TimelineMax()
-.fromTo(".prime-info",2,{
-  opacity:0,
-  scale:0
-},{
-  opacity:1,
-  scale:1, 
-  ease:"Linear.easeOut"
-})
-.fromTo(".prime-img",2,{
-  opacity:0,
-  x:"100%",
-  rotate:"180deg"
-},{
-  opacity:1,
-  rotate:"0deg",
-  x:"0%", 
-  ease:"Linear.easeOut"
-})
-
-
-const scenesquareFormula = new ScrollMagic.Scene({
-  triggerHook:0.5,
-  triggerElement:".main-section", 
-  duration:300
-})
-.setTween(formulas)
-.addTo(controller) 
-
-const sceneEvenOddFormula = new ScrollMagic.Scene({
-  triggerHook:0.5,
-  triggerElement:".even-odd-section", 
-  duration:300
-})
-.setTween(oddPrimeFormula)
+.setTween(exampleTitle) 
+.setClassToggle(".example-formula-container","sideline")
 .addTo(controller)
 
-const scenePrimeFormula = new ScrollMagic.Scene({
-  triggerHook:0.5, 
-  triggerElement:".prime-section",
-  duration:300
+/*END OF EXAMPLE TITLE TWEENING */
+
+/* START OF EVEN ODD SECTION TWEENING */
+
+const evenOdd = new TimelineMax()
+.fromTo(".even-odd",0.3,{
+  x:"-100%",
+  opacity:0,
+  duration:0.3
+},{
+  x:"0%", 
+  opacity:1,
+  duration:0.3
 })
-.setTween(primeFormula)
+
+.fromTo(".even-odd-info",0.3,{
+  y:"150%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+.fromTo(".wave-even-odd",0.3,{
+  y:"-100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out"
+})
+
+
+const evenOddImg = new TimelineMax()
+.fromTo(".dice",0.3,{
+  rotate:"180deg", 
+  ease:"power2.out", 
+  duration:0.3
+},{
+  rotate:"0deg", 
+  ease:"power2.out", 
+})
+
+.fromTo(".even-odd-link",0.3,{
+  y:"100%",
+  opacity:0,
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  opacity:1,
+  ease:"power2.out", 
+  duration:0.3
+})
+
+.fromTo(".bottom-wave-even-odd",0.3,{
+  y:"100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+const evenOddTween = new ScrollMagic.Scene({
+  triggerHook:1,
+  triggerElement:".even-odd-body-section", 
+  duration:600
+})
+.setTween(evenOdd)
 .addTo(controller)
+
+const imgEvenOddTween = new ScrollMagic.Scene({
+  triggerHook:0.7,
+  triggerElement:".img-even-odd-container",
+  duration:500
+})
+.setTween(evenOddImg)
+.addTo(controller)
+
+/* END OF EVEN ODD TWEENING */
+
+/* START OF PRIME NUMBER TWEENING */
+
+const prime = new TimelineMax()
+.fromTo(".prime",0.3,{
+  x:"120%",
+  ease:"power2.out", 
+  duration:0.3,
+},{
+  x:"0%", 
+  delay:1,
+  ease:"power2.out"
+})
+
+.fromTo(".wave-prime",0.3,{
+  y:"-100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out"
+})
+
+.fromTo(".prime-info",0.3,{
+  x:"-150%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  x:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+const primeTween = new ScrollMagic.Scene({
+  triggerElement:".prime-body-section", 
+  triggerHook:1,
+  duration:600
+})
+.setTween(prime)
+.addTo(controller)
+
+const primeImg = new TimelineMax()
+.fromTo(".billiard",0.3,{
+  rotate:"360deg", 
+  ease:"power2.out", 
+  duration:0.3
+},{
+  rotate:"0deg", 
+  delay:1,
+  ease:"power2.out", 
+}) 
+
+.fromTo(".prime-link",0.3,{
+  x:"100%",
+  opacity:0,
+  ease:"power2.out", 
+  duration:0.3
+},{
+  x:"0%",
+  opacity:1,
+  ease:"power2.out", 
+  duration:0.3
+})
+
+.fromTo(".bottom-waves-prime",0.3,{
+  y:"100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+const primeImgScene = new ScrollMagic.Scene({
+  triggerElement:".img-prime-container",
+  triggerHook:1, 
+  duration:600
+})
+.setTween(primeImg)
+.addTo(controller)
+/* END OF PRIME NUMBER SECTION */
+
+/* START OF RECTANGLE AREA TWEENING */
+
+const rectangle = new TimelineMax()
+.fromTo(".rectangle",0.3,{
+  x:"-100%",
+  opacity:0,
+  duration:0.3
+},{
+  x:"0%", 
+  opacity:1,
+  duration:0.3
+})
+
+.fromTo(".rectangle-info",0.3,{
+  y:"150%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+.fromTo(".wave-rectangle",0.3,{
+  y:"-100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out"
+}) 
+
+const rectangleScene = new ScrollMagic.Scene({
+  triggerHook:0.9,
+  triggerElement:".rectangle-section", 
+  duration:600
+})
+.setTween(rectangle)
+.addTo(controller)
+
+const rectangleImg = new TimelineMax()
+
+.fromTo(".rectangle-img",0.3,{
+  rotate:"180deg", 
+  ease:"power2.out", 
+  duration:0.3
+},{
+  rotate:"0deg", 
+  ease:"power2.out", 
+})
+
+.fromTo(".rectangle-link",0.3,{
+  y:"100%",
+  opacity:0,
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  opacity:1,
+  ease:"power2.out", 
+  duration:0.3
+})
+
+.fromTo(".bottom-wave-rectangle",0.3,{
+  y:"100%",
+  ease:"power2.out", 
+  duration:0.3
+},{
+  y:"0%",
+  ease:"power2.out", 
+  duration:0.3
+})
+
+const rectangleImgTween = new ScrollMagic.Scene({
+  triggerElement:".img-rectangle-container", 
+  triggerHook:1, 
+  duration:500
+})
+.setTween(rectangleImg)
+.addTo(controller)
+
+const scrollTop = TweenMax.to(".back-top",1,{
+  display:"flex", 
+  ease:"power2.out"
+})
+
+const scrollTopTween = new ScrollMagic.Scene({
+  triggerHook:0,
+  triggerElement:".example-formula-container", 
+  duration:500
+})
+.setTween(scrollTop)
+.addTo(controller)
+
+scrollTopBtn.addEventListener("click",() => {
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  })
+})
+
+window.addEventListener("DOMContentLoaded",() => {
+  fetch(`http://numbersapi.com/random/trivia?json`)
+.then(res => res.json())
+.then(data => facts.textContent = data.text)
+})
